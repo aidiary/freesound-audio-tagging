c859273c68f088854e232d490f56b77d6defdebc
aidiary <f2forest@gmail.com>
2018-05-22 11:04:20 +0900

    Add experimental settings

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
-python train.py --log_dir=logs/log.1 --seed 1234
+# 001: LeNet, Adam(0.001), epochs=32
+python train.py --log_dir=logs/log.001 --seed 1234
+
+# upload log
 tar cvzf logs_freesound.tar.gz logs
 gdrive upload logs_freesound.tar.gz
 sudo shutdown -h now
