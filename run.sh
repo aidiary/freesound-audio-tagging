5c83e60e23f49490e2ce815e8cc814c722cf9360
aidiary <f2forest@gmail.com>
2018-05-22 09:28:41 +0900

    Remove old models

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
-#python train.py --log_dir=logs/log.1 --seed 1234
-python train.py --log_dir=logs/log.2 --seed 1234 --arch VGG11
+python train.py --log_dir=logs/log.1 --seed 1234
 tar cvzf logs_freesound.tar.gz logs
 gdrive upload logs_freesound.tar.gz
 sudo shutdown -h now
