3165b3e15c31bfaef328ae4e94c2434ec757a722
aidiary <f2forest@gmail.com>
2018-05-22 11:09:25 +0900

    Update settings

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 python train.py --log_dir=logs/log.001 --seed 1234
 
 # upload log
-tar cvzf logs_freesound.tar.gz logs
-gdrive upload logs_freesound.tar.gz
+tar cvzf logs.001.tar.gz logs/logs.001
+gdrive upload logs.001.tar.gz
 sudo shutdown -h now
