1ecdd5a839fbd751dc3078df23328c169994961e
aidiary <f2forest@gmail.com>
2018-06-10 17:52:42 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,5 @@
-# 001: LeNet, Adam(0.001), epochs=32
-#python train.py --log_dir=logs/log.020.1 --seed 1234 --epochs 100
-python train.py --log_dir=logs/log.020.2 --seed 1235 --epochs 100
-python train.py --log_dir=logs/log.020.3 --seed 1236 --epochs 100
-python train.py --log_dir=logs/log.020.4 --seed 1237 --epochs 100
-python train.py --log_dir=logs/log.020.5 --seed 1238 --epochs 100
+python train.py --log_dir=logs/log.030 --seed 1234 --epochs 100
 
-# upload log
-tar cvzf logs.020.tar.gz logs/log.020.*
-gdrive upload logs.020.tar.gz
+tar cvzf log.030.tar.gz logs/log.030
+gdrive upload log.030.tar.gz
 sudo shutdown -h now
