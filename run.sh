6d48235085df54f56af385cdd5690f6251c599f5
aidiary <f2forest@gmail.com>
2018-06-11 13:09:15 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-python train.py --log_dir=logs/log.030 --seed 1234 --epochs 100
+python train.py --log_dir=logs/log.033 --seed 1234 --epochs 100
 
-tar cvzf log.030.tar.gz logs/log.030
-gdrive upload log.030.tar.gz
+tar cvzf log.033.tar.gz logs/log.033
+gdrive upload log.033.tar.gz
 sudo shutdown -h now
