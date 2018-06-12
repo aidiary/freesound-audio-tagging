5a0a3978bd4d319adee4f399c22e1a1cb4df32fe
aidiary <f2forest@gmail.com>
2018-06-12 17:11:15 +0900

    Add VGGish model

--- model.py
+++ model.py
@@ -1,50 +1,54 @@
 import torch.nn as nn
 
 
-class AlexNet2d(nn.Module):
+class VGGish(nn.Module):
 
     def __init__(self, num_classes):
-        super(AlexNet2d, self).__init__()
+        super(VGGish, self).__init__()
 
         self.num_classes = 41
 
         self.block1 = nn.Sequential(
-            nn.Conv2d(1, 96, kernel_size=11, stride=(2, 3), padding=5),
+            nn.Conv2d(1, 64, kernel_size=(3, 3), stride=(1, 1), padding=1),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=(1, 2)),
-            nn.BatchNorm2d(96)
+            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
         )
 
         self.block2 = nn.Sequential(
-            nn.Conv2d(96, 256, kernel_size=5, stride=(2, 3), padding=2),
+            nn.Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1), padding=1),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=(2, 2)),
-            nn.BatchNorm2d(256)
+            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
         )
 
         self.block3 = nn.Sequential(
-            nn.Conv2d(256, 384, kernel_size=3, padding=1),
+            nn.Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1), padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(384, 384, kernel_size=3, padding=1),
+            nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+        )
+
+        self.block4 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1), padding=1),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=(1, 2)),
-            nn.BatchNorm2d(256)
+            nn.Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
         )
 
         self.classifier = nn.Sequential(
-            nn.Linear(256 * 5 * 4, 2048),
+            nn.Linear(512 * 4 * 25, 4096),
             nn.ReLU(inplace=True),
-            nn.Linear(2048, 2048),
+            nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
-            nn.Linear(2048, self.num_classes)
+            nn.Linear(4096, self.num_classes)
         )
 
     def forward(self, x):
         x = self.block1(x)
         x = self.block2(x)
         x = self.block3(x)
+        x = self.block4(x)
         x = x.view(x.size(0), -1)
         x = self.classifier(x)
         return x
