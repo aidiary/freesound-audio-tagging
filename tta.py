e2a4036911f063f8b9fb45a77f63d149bd3bb692
aidiary <f2forest@gmail.com>
2018-06-19 16:38:33 +0900

    Add command line options

--- tta.py
+++ tta.py
@@ -7,7 +7,7 @@ from sklearn.preprocessing import LabelEncoder
 import torch
 from train import test_time_augmentation
 from dataset import AudioDataset
-from model import AlexNet, LeNet
+from model import AlexNet2d, AlexNet1d
 
 
 cuda = torch.cuda.is_available()
@@ -19,9 +19,18 @@ device = torch.device('cuda' if cuda else 'cpu')
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('log_dir')
-    parser.add_argument('model')
+    parser.add_argument('log_dir', type=str,
+                        help='input log directory')
+    parser.add_argument('model_file', type=str,
+                        help='input model file')
+    parser.add_argument('--conv_type', type=str,
+                        choices=['1d', '2d'], default='2d',
+                        help='convolution type of the model')
     args = parser.parse_args()
+
+    print('log_dir:', args.log_dir)
+    print('model_file:', args.model_file)
+    print('conv_type:', args.conv_type)
 
     # load dataset
     train_df = pd.read_csv('./data/train.csv')
@@ -36,8 +45,18 @@ def main():
     test_loader = torch.utils.data.DataLoader(test_dataset, 128, shuffle=False)
 
     # load model
-    model = AlexNet(num_classes).to(device)
-    model.load_state_dict(torch.load(args.model, map_location=lambda storage, loc: storage))
+    if args.conv_type == '2d':
+        model = AlexNet2d(num_classes).to(device)
+    elif args.conv_type == '1d':
+        model = AlexNet1d(num_classes).to(device)
+    else:
+        print('Invalid conv_type: %s' % args.conv_type)
+        exit(1)
+
+    print(model)
+
+    # 学習済みモデルをロード
+    model.load_state_dict(torch.load(args.model_file, map_location=lambda storage, loc: storage))
 
     # test time augmentation
     tta_predictions = test_time_augmentation(test_loader, model, num_aug=5)
