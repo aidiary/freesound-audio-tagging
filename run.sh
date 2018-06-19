684d066f7c00250679cf28c77684da04cc710270
aidiary <f2forest@gmail.com>
2018-06-20 08:40:45 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,18 @@
-#python train.py --log_dir=logs/log.034 --seed 1234 --epochs 100 --feature mfcc --conv_type 2d
-python train.py --log_dir=logs/log.038 --seed 1234 --epochs 100 --feature mfcc --conv_type lstm
-#python train.py --log_dir=logs/log.036 --seed 1234 --epochs 100 --feature melgram --conv_type 2d
-python train.py --log_dir=logs/log.039 --seed 1234 --epochs 100 --feature melgram --conv_type lstm
+python train.py --log_dir=logs/log.040 --seed 1234 --epochs 300 --feature mfcc --conv_type 2d
+python train.py --log_dir=logs/log.041 --seed 1234 --epochs 300 --feature mfcc --conv_type 1d
+python train.py --log_dir=logs/log.042 --seed 1234 --epochs 300 --feature melgram --conv_type 2d
+python train.py --log_dir=logs/log.043 --seed 1234 --epochs 300 --feature melgram --conv_type 1d
+python train.py --log_dir=logs/log.044 --seed 1234 --epochs 300 --feature mfcc --conv_type lstm
+python train.py --log_dir=logs/log.045 --seed 1234 --epochs 300 --feature melgram --conv_type lstm
 
 # test time augmentation
-#python tta.py logs/log.034 logs/log.034/epoch082-1.128-0.776.pth --feature mfcc --conv_type 2d
-#python tta.py logs/log.035 logs/log.035/epoch080-1.223-0.741.pth --feature mfcc --conv_type 1d
-#python tta.py logs/log.036 logs/log.036/epoch099-1.332-0.707.pth --feature melgram --conv_type 2d
-#python tta.py logs/log.037 logs/log.037/epoch099-1.597-0.637.pth --feature melgram --conv_type 1d
+# python tta.py logs/log.040 logs/log.040/.pth --feature mfcc --conv_type 2d
+# python tta.py logs/log.041 logs/log.041/.pth --feature mfcc --conv_type 1d
+# python tta.py logs/log.042 logs/log.042/.pth --feature melgram --conv_type 2d
+# python tta.py logs/log.043 logs/log.043/.pth --feature melgram --conv_type 1d
+# python tta.py logs/log.044 logs/log.044/.pth --feature mfcc --conv_type lstm
+# python tta.py logs/log.045 logs/log.045/.pth --feature melgram --conv_type lstm
 
-tar cvzf log.tar.gz logs/log.034 logs/log.035 logs/log.036 logs/log.037
+tar cvzf log.tar.gz logs/log.40 logs/log.041 logs/log.042 logs/log.043 logs/log.044 logs/log.045
 gdrive upload log.tar.gz
 sudo shutdown -h now
